c4068da5fd83c917a02be4793120960a4134d62d
macg30 <macg30@users.noreply.github.com>
2021-06-22 22:05:27 +0200

    states and events on components

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
-const Button = props => {
-    const { text , text2 , text3 } = props;
-    return (
-        <div>
-            <button type="button">{props.text}</button>
-            <button type="button">{text}</button>
+class Button extends React.Component {
+    state = {
+        count: 0,
+    }
+    handleClick = () => {
+        this.setState({
+            count : this.state.count +1,
+        })
+    }
+
+    render() {
+        const { count } = this.state;
+        return(
+            <div>
+                <h1>Manzanas: {count} </h1>
+            <button type="button" onClick={this.handleClick}>
+                Sumar
+            </button>
         </div>
-    )
-};
+        )
+    }
+}
 
 export default Button
